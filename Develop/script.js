f76117404edee13cc804e083c358e1bd9f8021ea
crams <cpramsey112@gmail.com>
2023-02-21 16:34:05 -0500

    added functions for selecting pword preferences

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,62 @@
 // Assignment code here
 
+// create arrays for available charaters 
+
+// create propmt to ask for user length and character type preferences 
+
+// create function to take preferences and generate password with available characters 
+
+// display pword 
+
 // availiable character arrays
 
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numericList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "=", ">", "<", "?", "@", "^", "_", "`", "{", "|", "{", "~"]
 
 // empty array to add pw to 
 var userPW = [];
+
+// create function for taking user length preference 
+
+function generatePassword() {
+
+  var pwLength = parseInt(prompt("Set desired length for password between 8 and 128 characters."));
+
+  if (pwLength < 8 || pwLength > 128 || isNaN(pwLength) === true) {
+    alert("Nah dawg I said between 8 and 128! wyd??");
+    return generatePassword();
+  }
+}
+
+// create function for taking character type preferences 
+
+  function promptUser() {
+
+    // ask if they want to include capital letters
+    var confirmCapital = confirm("Would you like capital letters in your pword?");
+    pwCriteria.includeUpper = confirmCapital;
+  
+    // ask if they want to include lowercase letters
+    var confirmLower = confirm("How bout some lower case?");
+    pwCriteria.includeLower = confirmLower;
+  
+    // ask if they want to include numbers
+    var confirmNum = confirm("How do you feel about some numbers?");
+    pwCriteria.includeNum = confirmNum;
+  
+    // ask if they want to include special characters
+    var confirmSpecial = confirm("And lastly, special characters?");
+    pwCriteria.includeSpecial = confirmSpecial;
+  
+    // check if the values all equal false - if so will rerun user prompts
+    if (pwCriteria.includeUpper === false && pwCriteria.includeLower === false && pwCriteria.includeNum === false && pwCriteria.includeSpecial === false) {
+      alert("We really got to do this again? Alright then, damn.");
+      return promptUser();
+    }
+}
+
 
 
 
